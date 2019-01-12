217b154064d131d6ab27d082d93c4c56fb61c6b6
imppomi <yoojk150827@gmail.com>
2019-01-12 15:14:06 +0900

    스플래시, 로그인 페이지 작성 완료

--- js/common.js
+++ js/common.js
@@ -4,3 +4,16 @@
 $(document).on('click', 'a[href="#"]', function(e){
     e.preventDefault();
 });
+//모바일 스플래쉬 화면
+// $(function(){
+//     setTimeout(function(){
+//         $('#splash').hide();
+//     }, 8000);
+// });
+//로그인 통과
+// $(function(){
+//     $('#get_start').on('click', function(e){
+//         e.preventDefault();
+//         $('#login').hide();
+//     });
+// });
